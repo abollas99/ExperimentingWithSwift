3f906330f6c32bbeeb936ff56f6aedcecaef0d56
Anthony Bollas <MY_NAME@example.com>
2022-02-11 12:29:50 -0500

    First Commit Test

---
+++ FirstTest/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Anthony Bollas on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
